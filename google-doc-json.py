58a23d5829fbf00ab3dbbf16d8aa2134670172da
chagan <chrishagan23@gmail.com>
2015-01-04 09:12:14 -0600

    Removed csvkit, csv now exports to json with top-level key for use with handlebars.js

--- google-doc-json.py
+++ google-doc-json.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 from gdocs import GoogleDoc
-import glob, subprocess
+import glob, subprocess, json, csv
 
 url = 'https://docs.google.com/spreadsheet/ccc?key=0AkY5o_aAkwFydEctaXllbzlGUGRLanVQc0hSMlo5YXc'
     
@@ -21,8 +21,21 @@ def get_doc(url):
 
 def gdoc_to_json():
 	for files in glob.glob("*.csv"):
-		print files
-		subprocess.call('csvjson %s > output.json' % files, shell=True) 
+		# print files
+		subprocess.call('csvjson %s > output_csvkit.json' % files, shell=True)
+		csv_to_json(files)
+
+def csv_to_json(source):
+	output = []
+	with open(source) as csvfile:
+		reader = csv.DictReader(csvfile)
+		for row in reader:
+			output.append(row)
+
+	final_json = {'cards': output}
+	
+	with open('output.json', 'w') as outfile:
+		json.dump(final_json, outfile)
 
 
 if __name__ == "__main__":
